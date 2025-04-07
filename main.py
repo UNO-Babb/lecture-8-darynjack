58b11516f808ea1351c04249e5d1cb1ad4cf6763
darynjack <darynjackson@unomaha.edu>
2025-04-07 16:06:45 +0000

    yeah

--- main.py
+++ main.py
@@ -1,43 +1,35 @@
 def main():
-  pitching = open("MLB_Pitching.csv", 'r')
-  
-  for line in myFile:
+  team_data = []
+
+  # Read pitching data
+  with open("MLB_Pitching.csv", 'r') as pitching:
+   for line in pitching:
     info = line.split(",")
-    name = info [0]
+    name = info[0]
     runs_allowed = info[3]
     wins = info[4]
-    losses = info [5]
-    era = info [7]
+    losses = info[5]
+    era = info[7]
 
-    team_data.append = [name, "" , runs_allowed, wins, losses, era]
-    
-   myFile.close()
+  team_data.append([name, "", runs_allowed, wins, losses, era])
 
-
-  hitting = open("MLB_Hitting.csv", 'r')
-
-  teamCount = 0
-  for line in hitting:
-    info = line.spit(",")
+    # Read hitting data
+  with open("MLB_Hitting.csv", 'r') as hitting:
+   teamCount = 0
+   for line in hitting:
+    info = line.split(",")
     runs_scored = info[3]
     print(runs_scored)
     team_data[teamCount][1] = runs_scored
     print(team_data[teamCount])
 
-    teamCount = teamCount +1
+    teamCount += 1
 
-   hitting.close()
-
-
-
-
-  outFile = open("mlb_output.csv", 'w')
-
-  #process each line of the team_data and save to the output file.
-  output = ""
-  outFile.write(output)
-
-  outFile.close()
+    # Write output to file
+  with open("mlb_output.csv", 'w') as outFile:
+    for team in team_data:
+      output = ",".join(team) + "\n"
+      outFile.write(output)
 
 if __name__ == '__main__':
-  main()
+    main()
