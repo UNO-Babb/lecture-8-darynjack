bc483389bec13c1889137836605e4037f7ab56f0
darynjack <darynjackson@unomaha.edu>
2025-04-07 16:29:40 +0000

    ye

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 def main():
-  team_data = []
+ team_data = []
 
-  # Read pitching data
-  with open("MLB_Pitching.csv", 'r') as pitching:
-   for line in pitching:
-    info = line.split(",")
+ # Read pitching data
+ with open("MLB_Pitching.csv", 'r') as pitching:
+  for line in pitching:
+    info = line.strip().split(",")
     name = info[0]
     runs_allowed = info[3]
     wins = info[4]
     losses = info[5]
     era = info[7]
-
-  team_data.append([name, "", runs_allowed, wins, losses, era])
+      # Append data for the pitching team
+    team_data.append([name, "", runs_allowed, wins, losses, era])
 
     # Read hitting data
   with open("MLB_Hitting.csv", 'r') as hitting:
-   teamCount = 0
-   for line in hitting:
-    info = line.split(",")
-    runs_scored = info[3]
-    print(runs_scored)
-    team_data[teamCount][1] = runs_scored
-    print(team_data[teamCount])
-
-    teamCount += 1
+    teamCount = 0
+    for line in hitting:
+      info = line.strip().split(",")
+      runs_scored = info[3]
+    # Update runs scored for the corresponding team
+      team_data[teamCount][1] = runs_scored
+      teamCount += 1
 
     # Write output to file
   with open("mlb_output.csv", 'w') as outFile:
